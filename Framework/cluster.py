d54f9e26cf75b02022950c67818cfba9327fd5ed
koralp çatalsakal <mrkoralp@gmail.com>
2019-10-15 21:29:34 +0200

    Plotting function added. Both clustering and plotting currently works

--- Framework/cluster.py
+++ Framework/cluster.py
@@ -6,7 +6,25 @@ Date : 15/10/2019
 
 def clusterData(model_func = None, data = None , label = None):
 
+    """
+    Model agnostic method to be used in clustering. Can be used to try different models over datasets.
+    Default setup accepts unsupervised dataset.
+
+    Args:
+        model_func(Model.function) : The specific model function to be used. The result of the function is
+            returned in the model
+
+        data(numpy.ndarray or pandas.DataFrame) : Data to be used in training(Without the ground truth data)
+
+        label(numpy.ndarray or pandas.DataFrame) : Labels(Ground truth) of the data argument.
+
+    Returns:
+        result(Model) : Returns the fitted/trained model as output
+
+
+    """
     if label == None:
-        model_func(data)
+        result = model_func(data)
     else:
-        model_func(data,label)
+        result = model_func(data,label)
+    return result
