38aa2c21c439474f4365a9ac2c3715ce23e47c65
koralp çatalsakal <mrkoralp@gmail.com>
2019-10-30 15:11:41 +0100

    Train test split implemented Correct metric for calculating ensemble rmse implemented

--- Framework/cluster.py
+++ Framework/cluster.py
@@ -4,6 +4,8 @@ Date : 15/10/2019
 """
 
 import xgboost
+import numpy
+import pandas
 
 def clusterData(model_func = None, data = None , label = None):
 
@@ -38,7 +40,10 @@ def splitDataLabeled(nClusters,data,cluster_labels):
 
     data_dict = {}
     for i in range(nClusters):
-        data_dict['cluster{0}'.format(i)] = data[cluster_labels == i]
+        if type(data) == numpy.ndarray:
+            data_dict['cluster{0}'.format(i)] = pandas.DataFrame(data[cluster_labels == i])
+        else:
+            data_dict['cluster{0}'.format(i)] = data[cluster_labels == i]
     return data_dict
 
 def convertOriginalData(data_dict,X,y):
@@ -46,7 +51,17 @@ def convertOriginalData(data_dict,X,y):
     for i, (key, val) in enumerate(data_dict.items()):
         #print(val.index.values)
         original_data_split['original_data_cluster{0}'.format(i)] = X.iloc[val.index.values]
-        original_data_split['original_label_cluster{0}'.format(i)] = y[val.index.values]
+        temp = X.loc[original_data_split['original_data_cluster{0}'.format(i)].index]
+        original_data_split['original_train_cluster{0}'.format(i)] = temp[temp['instance'] == 'train']
+        original_data_split['original_test_cluster{0}'.format(i)] = temp[temp['instance'] == 'test']
+        original_data_split['original_train_cluster{0}'.format(i)] = original_data_split['original_train_cluster{0}'.format(i)].drop(columns = 'instance')
+        original_data_split['original_test_cluster{0}'.format(i)] = original_data_split['original_test_cluster{0}'.format(i)].drop(columns= 'instance')
+        original_data_split['original_label_cluster{0}'.format(i)] = y.iloc[val.index.values]
+        temp = y.loc[original_data_split['original_label_cluster{0}'.format(i)].index]
+        original_data_split['original_train_label_cluster{0}'.format(i)] = temp[temp['instance'] == 'train']
+        original_data_split['original_test_label_cluster{0}'.format(i)] = temp[temp['instance'] == 'test']
+        original_data_split['original_train_label_cluster{0}'.format(i)] = original_data_split['original_train_label_cluster{0}'.format(i)].drop(columns = 'instance')
+        original_data_split['original_test_label_cluster{0}'.format(i)] = original_data_split['original_test_label_cluster{0}'.format(i)].drop(columns = 'instance')
     return original_data_split
 
 def trainMultipleModels(model_func,data_dict,option,params,**kwargs):
@@ -54,10 +69,10 @@ def trainMultipleModels(model_func,data_dict,option,params,**kwargs):
     model_dict = {}
     eval_dict = {}
     if option == 'XGBoost':
-        for i in range(len(data_dict.items())//2):
+        for i in range(len(data_dict.items())//6):
             kwargs['evals_result'] = {}
-            dtrain = xgboost.DMatrix(data_dict['original_data_cluster{}'.format(i)],label = data_dict['original_label_cluster{}'.format(i)])
-            eval = [(xgboost.DMatrix(data_dict['original_data_cluster{0}'.format(i)], label=data_dict['original_label_cluster{0}'.format(i)]), "train")]
+            dtrain = xgboost.DMatrix(data_dict['original_train_cluster{0}'.format(i)],label = data_dict['original_train_label_cluster{0}'.format(i)])
+            eval = [(xgboost.DMatrix(data_dict['original_train_cluster{0}'.format(i)], label=data_dict['original_train_label_cluster{0}'.format(i)]), "train"),(xgboost.DMatrix(data_dict['original_test_cluster{0}'.format(i)], label=data_dict['original_test_label_cluster{0}'.format(i)]), "test")]
             model_dict['model{0}'.format(i)] = model_func(params,dtrain,evals = eval,**kwargs)
             eval_dict['eval{0}'.format(i)] = kwargs['evals_result']
     else:
