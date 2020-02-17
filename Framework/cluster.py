54d77675c76d8c72ace39db6cf8cb7a9a6e7bc41
koralp çatalsakal <mrkoralp@gmail.com>
2020-02-17 23:08:43 +0100

    Framework change

--- Framework/cluster.py
+++ Framework/cluster.py
@@ -6,6 +6,8 @@ Date : 15/10/2019
 import xgboost
 import numpy
 import pandas
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
 
 def clusterData(model_func = None, data = None , label = None):
 
@@ -47,37 +49,76 @@ def splitDataLabeled(nClusters,data,cluster_labels):
             data_dict['cluster{0}'.format(i)] = data[cluster_labels == i]
     return data_dict
 
-def convertOriginalData(data_dict,X,y):
+def convertOriginalData(data_dict,X,y,no_val = False):
     original_data_split = {}
-    for i, (key, val) in enumerate(data_dict.items()):
-        #print(val.index.values)
-        original_data_split['original_data_cluster{0}'.format(i)] = X.iloc[val.index.values]
-        temp = X.loc[original_data_split['original_data_cluster{0}'.format(i)].index]
-        original_data_split['original_train_cluster{0}'.format(i)] = temp[temp['instance'] == 'train']
-        original_data_split['original_test_cluster{0}'.format(i)] = temp[temp['instance'] == 'test']
-        original_data_split['original_train_cluster{0}'.format(i)] = original_data_split['original_train_cluster{0}'.format(i)].drop(columns = 'instance')
-        original_data_split['original_test_cluster{0}'.format(i)] = original_data_split['original_test_cluster{0}'.format(i)].drop(columns= 'instance')
-        original_data_split['original_label_cluster{0}'.format(i)] = y.iloc[val.index.values]
-        temp = y.loc[original_data_split['original_label_cluster{0}'.format(i)].index]
-        original_data_split['original_train_label_cluster{0}'.format(i)] = temp[temp['instance'] == 'train']
-        original_data_split['original_test_label_cluster{0}'.format(i)] = temp[temp['instance'] == 'test']
-        original_data_split['original_train_label_cluster{0}'.format(i)] = original_data_split['original_train_label_cluster{0}'.format(i)].drop(columns = 'instance')
-        original_data_split['original_test_label_cluster{0}'.format(i)] = original_data_split['original_test_label_cluster{0}'.format(i)].drop(columns = 'instance')
+    if no_val:
+        for i, (key, val) in enumerate(data_dict.items()):
+            #print(val.index.values)
+            original_data_split['cluster{0}'.format(i)] = X.iloc[val.index.values]
+            #original_data_split['cluster{0}'.format(i)] = original_data_split['cluster{0}'.format(i)].drop(columns = 'instance')
+
+            original_data_split['label_cluster{0}'.format(i)] = y.iloc[val.index.values]
+            #original_data_split['label_cluster{0}'.format(i)] = original_data_split['label_cluster{0}'.format(i)].drop(columns = 'instance')
+    else:
+        for i, (key, val) in enumerate(data_dict.items()):
+            #print(val.index.values)
+            original_data_split['original_data_cluster{0}'.format(i)] = X.iloc[val.index.values]
+            temp = X.loc[original_data_split['original_data_cluster{0}'.format(i)].index]
+            original_data_split['original_train_cluster{0}'.format(i)] = temp[temp['instance'] == 'train']
+            original_data_split['original_test_cluster{0}'.format(i)] = temp[temp['instance'] == 'test']
+            original_data_split['original_train_cluster{0}'.format(i)] = original_data_split['original_train_cluster{0}'.format(i)].drop(columns = 'instance')
+            original_data_split['original_test_cluster{0}'.format(i)] = original_data_split['original_test_cluster{0}'.format(i)].drop(columns= 'instance')
+            original_data_split['original_label_cluster{0}'.format(i)] = y.iloc[val.index.values]
+            temp = y.loc[original_data_split['original_label_cluster{0}'.format(i)].index]
+            original_data_split['original_train_label_cluster{0}'.format(i)] = temp[temp['instance'] == 'train']
+            original_data_split['original_test_label_cluster{0}'.format(i)] = temp[temp['instance'] == 'test']
+            original_data_split['original_train_label_cluster{0}'.format(i)] = original_data_split['original_train_label_cluster{0}'.format(i)].drop(columns = 'instance')
+            original_data_split['original_test_label_cluster{0}'.format(i)] = original_data_split['original_test_label_cluster{0}'.format(i)].drop(columns = 'instance')
+
     return original_data_split
 
-def trainMultipleModels(model_func,data_dict,option,params,**kwargs):
+def trainMultipleModels(model_func,data_dict,option,params,no_val = False,**kwargs):
 
     model_dict = {}
     eval_dict = {}
-    if option == 'XGBoost':
-        for i in range(len(data_dict.items())//6):
-            kwargs['evals_result'] = {}
-            dtrain = xgboost.DMatrix(data_dict['original_train_cluster{0}'.format(i)],label = data_dict['original_train_label_cluster{0}'.format(i)])
-            eval = [(xgboost.DMatrix(data_dict['original_train_cluster{0}'.format(i)], label=data_dict['original_train_label_cluster{0}'.format(i)]), "train"),(xgboost.DMatrix(data_dict['original_test_cluster{0}'.format(i)], label=data_dict['original_test_label_cluster{0}'.format(i)]), "test")]
-            model_dict['model{0}'.format(i)] = model_func(params,dtrain,evals = eval,**kwargs)
-            eval_dict['eval{0}'.format(i)] = kwargs['evals_result']
+    if no_val:
+        if option == 'XGBoost':
+            for i in range(len(data_dict)//2):
+                kwargs['evals_result'] = {}
+                dtrain = xgboost.DMatrix(data_dict['cluster{0}'.format(i)],label = data_dict['label_cluster{0}'.format(i)])
+                eval = [(xgboost.DMatrix(data_dict['cluster{0}'.format(i)], label=data_dict['label_cluster{0}'.format(i)]), "train")]
+                model_dict['model{0}'.format(i)] = xgboost.train(params,dtrain,evals = eval,**kwargs)
+                eval_dict['eval{0}'.format(i)] = kwargs['evals_result']
+        elif option == 'LinearRegressor':
+            for i in range(len(data_dict)//2):
+                model_dict['model{0}'.format(i)] = LinearRegression().fit(data_dict['cluster{0}'.format(i)], data_dict['label_cluster{0}'.format(i)],**kwargs)
+                eval_dict['eval{0}'.format(i)] = {'train': {'rmse': _calculate_accuracy(model_dict['model{0}'.format(i)].predict,data_dict['cluster{0}'.format(i)],data_dict['label_cluster{0}'.format(i)])}}
     else:
-        print( 'eeee')
-        #for i, (k,v) in enumerate(data_dict.items()):
-            #model_dict['model{0}'.format(i)] = model_func(kwargs['params'],data_dict['original_data_cluster{}'.format(i)], data_dict['original_label_cluster{}'.format(i)],kwargs)
+        if option == 'XGBoost':
+            for i in range(len(data_dict.items())//6):
+                kwargs['evals_result'] = {}
+                dtrain = xgboost.DMatrix(data_dict['original_train_cluster{0}'.format(i)],label = data_dict['original_train_label_cluster{0}'.format(i)])
+                eval = [(xgboost.DMatrix(data_dict['original_train_cluster{0}'.format(i)], label=data_dict['original_train_label_cluster{0}'.format(i)]), "train"),(xgboost.DMatrix(data_dict['original_test_cluster{0}'.format(i)], label=data_dict['original_test_label_cluster{0}'.format(i)]), "test")]
+                model_dict['model{0}'.format(i)] = xgboost.train(params,dtrain,evals = eval,**kwargs)
+                eval_dict['eval{0}'.format(i)] = kwargs['evals_result']
+        elif option == 'LinearRegressor':
+            for i in range(len(data_dict.items())//6):
+                model_dict['model{0}'.format(i)] = LinearRegression().fit(data_dict['original_train_cluster{0}'.format(i)], data_dict['original_train_label_cluster{0}'.format(i)],**kwargs)
+                eval_dict['eval{0}'.format(i)] = {'train': {'rmse': _calculate_accuracy(model_dict['model{0}'.format(i)].predict,data_dict['original_train_cluster{0}'.format(i)],data_dict['original_train_label_cluster{0}'.format(i)])},
+                'test': {'rmse': _calculate_accuracy(model_dict['model{0}'.format(i)].predict,data_dict['original_test_cluster{0}'.format(i)],data_dict['original_test_label_cluster{0}'.format(i)])}}
+
     return model_dict,eval_dict
+
+def mapDictToArray(cluster_dict,target_array):
+    test_dict = {}
+    for i in range(len(cluster_dict)):
+        test_dict['cluster{0}'.format(i)] = target_array[cluster_dict['cluster{0}'.format(i)].index]
+    return test_dict
+
+def mapTestToOriginal(data,labels,test_data,via = 'Shapley'):
+    knn = KNeighborsClassifier(n_neighbors=5).fit(data,labels)
+    test_labels = knn.predict(test_data)
+    return test_labels
+
+def _calculate_accuracy(func,X_test,y_test):
+    return numpy.sqrt(numpy.mean((func(X_test) - y_test)**2))
---
+++ Framework/datasets.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+import sys
+import shap
+
+#sys.path.append("..")
+
+def returnAlldatasets():
+    dataset_holder = [admissions(),amazon(),boston(),crime(),diabetes(),fish_weights(),insurance(),nhanes(),nye_airbnb(),student_grades()]
+    return dataset_holder
+
+def boston():
+    X,y = shap.datasets.boston()
+    name = 'Boston'
+    return X,y,name
+
+def diabetes():
+    X,y = shap.datasets.diabetes()
+    name = 'Diabetes'
+    return X,y,name
+
+def crime():
+    X,y = shap.datasets.communitiesandcrime()
+    name = 'Crime'
+    return X,y,name
+
+def nhanes():
+    X,y = shap.datasets.nhanesi()
+    name = 'Nhanes'
+    return X,y,name
+
+def fish_weights():
+    fish_df = pd.read_csv('../Data/Fish.csv')
+    X = fish_df.drop('Weight',axis = 1)
+    X_fac = pd.factorize(X['Species'])
+    X['Species'] = X_fac[0]
+    y = np.array(fish_df['Weight'])
+    name = 'Fish_weights'
+    return X,y,name
+
+def nye_airbnb():
+    nye_df = pd.read_csv('../Data/AB_NYC_2019.csv')
+    nye_df.drop(['id','name','host_id','host_name','latitude','longitude','last_review'],axis = 1,inplace = True)
+    nye_df['neighbourhood_group'] = pd.factorize(nye_df['neighbourhood_group'])[0]
+    nye_df['neighbourhood'] = pd.factorize(nye_df['neighbourhood'])[0]
+    nye_df['room_type'] = pd.factorize(nye_df['room_type'])[0]
+    X = nye_df.drop('price',axis = 1)
+    y = np.array(nye_df['price'])
+    name = 'NYE_Airbnb'
+    return X,y,name
+
+
+def admissions():
+    admission_df = pd.read_csv('../Data/Admission_Predict.csv')
+    X = admission_df.drop(['Serial No.', 'Chance of Admit '],axis = 1)
+    y = np.array(admission_df['Chance of Admit '])
+    name = 'Admissions'
+    return X,y,name
+
+
+def student_grades():
+    grades_df = pd.read_csv('../Data/student-mat.csv')
+    grades_df['school'] = pd.factorize(grades_df['school'])[0]
+    grades_df['sex'] = pd.factorize(grades_df['sex'])[0]
+    grades_df['address'] = pd.factorize(grades_df['address'])[0]
+    grades_df['famsize'] = pd.factorize(grades_df['famsize'])[0]
+    grades_df['Pstatus'] = pd.factorize(grades_df['Pstatus'])[0]
+    grades_df['Mjob'] = pd.factorize(grades_df['Mjob'])[0]
+    grades_df['Fjob'] = pd.factorize(grades_df['Fjob'])[0]
+    grades_df['reason'] = pd.factorize(grades_df['reason'])[0]
+    grades_df['guardian'] = pd.factorize(grades_df['guardian'])[0]
+    grades_df['schoolsup'] = pd.factorize(grades_df['schoolsup'])[0]
+    grades_df['famsup'] = pd.factorize(grades_df['famsup'])[0]
+    grades_df['paid'] = pd.factorize(grades_df['paid'])[0]
+    grades_df['activities'] = pd.factorize(grades_df['activities'])[0]
+    grades_df['nursery'] = pd.factorize(grades_df['nursery'])[0]
+    grades_df['higher'] = pd.factorize(grades_df['higher'])[0]
+    grades_df['internet'] = pd.factorize(grades_df['internet'])[0]
+    grades_df['romantic'] = pd.factorize(grades_df['romantic'])[0]
+    X = grades_df.drop('G3',axis = 1)
+    y = np.array(grades_df['G3'])
+    name = 'Student_grades'
+    return X,y,name
+
+
+def amazon():
+    amazon_df = pd.read_csv('../Data/amazon.csv')
+    amazon_df['state'] = pd.factorize(amazon_df['state'])[0]
+    amazon_df['month'] = pd.factorize(amazon_df['month'])[0]
+    X = amazon_df.drop(['date','number'],axis = 1)
+    y = np.array(amazon_df['number'])
+    name = 'Amazon'
+    return X,y,name
+
+
+def insurance():
+    insurance_df = pd.read_csv('../Data/insurance.csv')
+    insurance_df['sex'] = pd.factorize(insurance_df['sex'])[0]
+    insurance_df['smoker'] = pd.factorize(insurance_df['smoker'])[0]
+    insurance_df['region'] = pd.factorize(insurance_df['region'])[0]
+    X = insurance_df.drop('charges',axis = 1)
+    y = np.array(insurance_df['charges'])
+    name = 'Insurance'
+    return X,y,name
