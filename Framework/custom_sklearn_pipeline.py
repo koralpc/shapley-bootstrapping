49c01cd19f7fc537529ececb1a72bd6b0b15179e
koralp çatalsakal <mrkoralp@gmail.com>
2020-03-03 21:25:09 +0100

    New step : Shap->Cluster->Map->Reduce added to the notebooks

--- Framework/custom_sklearn_pipeline.py
+++ Framework/custom_sklearn_pipeline.py
@@ -21,6 +21,7 @@ from Framework import datasets
 from Framework import processing
 from sklearn.base import RegressorMixin
 from sklearn.base import BaseEstimator
+from sklearn.impute import SimpleImputer
 
 
 class CustomPipelineModel(BaseEstimator,RegressorMixin):
@@ -36,63 +37,62 @@ class CustomPipelineModel(BaseEstimator,RegressorMixin):
         self.ensemble_type = ensemble_type
         self.nClusters = nClusters
         self.shap_model = ShapleyModel(explainer_type,ensemble_type,nClusters,notebook_mode)
-        #model = CustomPipelineModel(notebook_mode,explainer_type,ensemble_type,nClusters)
+        self.imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
         self.pipeline = Pipeline(steps=[('Shapley-Blackbox',self)])
 
     def fit(self,X,y):
         X_train,shap_dataframe,original_split,y_org,original_split_shapley,y_shap,kmeans_original,kmeans_shapley = self.prepareData(X,y)
 
-        #model_dict,eval_results = blackbox_model.trainPredictor(original_split,y_org)
         self.original_labels = kmeans_original.labels_
         self.shapley_labels = kmeans_shapley.labels_
         self.X = X_train
         self.shap_values = shap_dataframe
         self.model_dict_shapley,self.eval_results_shapley = self.shap_model.trainPredictor(original_split_shapley,y_shap)
-
+        #Use if task is classification
+        self.classes_ = np.unique(y)
         return self
 
     def predict(self,X):
 
+        #X,_ = processing.clear_nan(X,None)
+        X = self.imputer.fit_transform(X)
         X = self.X_scaler.transform(X)
         shapley_test = self.shap_model.predictShapleyValues(X)
         shapley_test_df = pd.DataFrame(shapley_test)
-        print('Labels:' , self.shapley_labels)
-        data_dict_shapley_test= self.shap_model.clusterDataTest(self.shap_values,self.shapley_labels,shapley_test_df)
-        #data_dict_original_test= self.shap_model.clusterDataTest(self.X,self.original_labels,X)
 
-        print(data_dict_shapley_test)
+        data_dict_shapley_test= self.shap_model.clusterDataTest(self.shap_values,self.shapley_labels,shapley_test_df)
         original_split_shapley_test = self.shap_model.prepareTestData(data_dict_shapley_test,X,shapley_test_df)
-        y_pred = self.shap_model.predict(original_split_shapley_test,self.model_dict_shapley)
+
         predictions = original_split_shapley_test.apply(lambda x : self.shap_model.predictRow(x,self.model_dict_shapley),axis = 1)
         return predictions
 
 
+#Will be added in case of classification task addition
+"""    def predict_proba(self,X):
+        proba = self.predict(X)
+
+        proba = proba[:, :len(self.classes_)]
+        normalizer = proba.sum(axis=1)[:, np.newaxis]
+        normalizer[normalizer == 0.0] = 1.0
+        proba /= normalizer
+
+        return proba
+"""
+
+
     def prepareData(self,X,y):
 
+        X,y = processing.clear_nan(X,y)
         X_train,X_train_tr,X_train_val,X_test,y_train,y_train_tr,y_train_val,y_test,self.X_scaler = processing.prepare_pipeline_data(X,y)
 
-        # In[5]:
-
         shap_values = self.shap_model.explainShapley(X_train,y_train,X_train_tr,y_train_tr,X_train_val,y_train_val)
-
-        # In[6]:
-        X_instanced = pd.concat((X_train,pd.DataFrame(columns = ['instance'])),axis = 1)
-        X_instanced['instance'].iloc[X_train_tr.index] = 'train'
-        X_instanced['instance'].iloc[X_train_val.index] = 'val'
-        y_instanced = pd.concat((pd.DataFrame(y_train),pd.DataFrame(columns = ['instance'])),axis = 1)
-        y_instanced['instance'].iloc[X_train_tr.index] = 'train'
-        y_instanced['instance'].iloc[X_train_val.index] = 'val'
-
-
-        #Gather shapley values and output values in one dataframe
         shap_dataframe = pd.DataFrame(data = shap_values)
-        shap_instanced = pd.concat((shap_dataframe,pd.DataFrame(columns = ['instance'])),axis = 1)
-        shap_instanced['instance'].loc[X_train_tr.index] = 'train'
-        shap_instanced['instance'].loc[X_train_val.index] = 'val'
 
-        #Split the clusters into a dictionary
+        X_instanced,y_instanced,shap_instanced = processing.assign_instances(X_train,X_train_tr,X_train_val,y_train,shap_dataframe)
+
+        #Cluster the data
         data_dict,data_dict_original,kmeans_shapley,kmeans_original = self.shap_model.clusterData(X_train,shap_dataframe)
-        y_train_df = pd.DataFrame(y_train)
+        y_train_df = pd.DataFrame(y)
 
         original_split,y_org,original_split_shapley,y_shap = self.shap_model.prepareTrainData(data_dict,data_dict_original,X_instanced,y_instanced,shap_instanced,True)
 
--- Framework/processing.py
+++ Framework/processing.py
@@ -10,6 +10,24 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
 
+def assign_instances(X,X_train,X_val,y,shapley_values):
+
+    X = pd.DataFrame(X)
+
+    X_instanced = pd.concat((X,pd.DataFrame(columns = ['instance'])),axis = 1)
+    X_instanced['instance'].iloc[X_train.index] = 'train'
+    X_instanced['instance'].iloc[X_val.index] = 'val'
+    y_instanced = pd.concat((pd.DataFrame(y),pd.DataFrame(columns = ['instance'])),axis = 1)
+    y_instanced['instance'].iloc[X_train.index] = 'train'
+    y_instanced['instance'].iloc[X_val.index] = 'val'
+
+
+    #Gather shapley values and output values in one dataframe
+    shap_instanced = pd.concat((shapley_values,pd.DataFrame(columns = ['instance'])),axis = 1)
+    shap_instanced['instance'].loc[X_train.index] = 'train'
+    shap_instanced['instance'].loc[X_val.index] = 'val'
+    return X_instanced,y_instanced,shap_instanced
+
 def dimensional_reduce(method,data):
 
     transformed_data = method.fit_transform(data)
@@ -51,28 +69,28 @@ def prepare_pipeline_data(X,y,test_proportion = 0.2,validation_proportion = 0.25
 
 def prepare_pipeline_reduced_data(X,y,method,test_proportion = 0.2,validation_proportion = 0.25):
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_proportion, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_proportion, random_state=0)
 
-        X_train.reset_index(inplace = True)
-        X_train.drop(['index'],axis = 1,inplace = True)
-        X_test.reset_index(inplace = True)
-        X_test.drop(['index'],axis = 1,inplace = True)
+    X_train.reset_index(inplace = True)
+    X_train.drop(['index'],axis = 1,inplace = True)
+    X_test.reset_index(inplace = True)
+    X_test.drop(['index'],axis = 1,inplace = True)
 
-        X_train,_ = dimensional_reduce(method,X_train)
-        X_test,_ = dimensional_reduce(method,X_test)
+    X_train,_ = dimensional_reduce(method,X_train)
+    X_test,_ = dimensional_reduce(method,X_test)
 
-        scaler = preprocessing.StandardScaler().fit(X_train)
-        X_train = pd.DataFrame(scaler.transform(X_train))
-        X_test = pd.DataFrame(scaler.transform(X_test))
+    scaler = preprocessing.StandardScaler().fit(X_train)
+    X_train = pd.DataFrame(scaler.transform(X_train))
+    X_test = pd.DataFrame(scaler.transform(X_test))
 
-        scaler_y = preprocessing.StandardScaler().fit(y_train.reshape(-1,1))
+    scaler_y = preprocessing.StandardScaler().fit(y_train.reshape(-1,1))
 
-        y_train = scaler_y.transform(y_train.reshape(-1,1)).reshape(-1,)
-        y_test = scaler_y.transform(y_test.reshape(-1,1)).reshape(-1,)
+    y_train = scaler_y.transform(y_train.reshape(-1,1)).reshape(-1,)
+    y_test = scaler_y.transform(y_test.reshape(-1,1)).reshape(-1,)
 
-        X_train_tr,X_train_val,y_train_tr,y_train_val = train_test_split(X_train,y_train,test_size = validation_proportion,random_state = 0)
+    X_train_tr,X_train_val,y_train_tr,y_train_val = train_test_split(X_train,y_train,test_size = validation_proportion,random_state = 0)
 
-        return X_train,X_train_tr,X_train_val,X_test,y_train,y_train_tr,y_train_val,y_test
+    return X_train,X_train_tr,X_train_val,X_test,y_train,y_train_tr,y_train_val,y_test
 
 def reduce_after_clusters(method,data):
     data_attributes = data.iloc[:,:-2]
@@ -88,3 +106,24 @@ def reduce_after_clusters(method,data):
         pass
     transformed_data['cluster'] = data['cluster']
     return transformed_data,explained_variance
+
+def clear_nan(X,y):
+    if y is not None:
+        X = pd.DataFrame(X)
+        X.reset_index(inplace = True)
+
+        X.drop(['index'],axis = 1,inplace = True)
+        ## Dropping NA values
+        drop_indexes = np.unique(np.where(X.isna())[0])
+        X.dropna(inplace = True)
+        mask = np.ones(len(y),dtype = bool)
+        mask[drop_indexes] = False
+        y = y[mask]
+        return X,y
+    else:
+        X = pd.DataFrame(X)
+        X.reset_index(inplace = True)
+
+        X.drop(['index'],axis = 1,inplace = True)
+        X.dropna(inplace = True)
+        return X,None
